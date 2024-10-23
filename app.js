69d3b17f5c82ad2d767c9bca321476f615d30bc8
Mahmoud Reda <mahmoudreda457@gmail.com>
2024-10-23 11:03:57 +0300

    handle unhandled routes

--- app.js
+++ app.js
@@ -35,4 +35,12 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+// handle unhandled routes
+app.all('*', (req, res) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `Can't find ${req.originalUrl} on this server`
+    });
+});
+
 module.exports = app;
